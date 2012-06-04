d9184200dbaaf869586eb9b0193e17d2e26c8882
Clément LEFEVRE <clement.lefevre@9h37.fr>
2012-06-04 14:01:13 +0200

    Mise à jour du settings.py.

--- test_django_sentry/test_django_sentry/settings.py
+++ test_django_sentry/test_django_sentry/settings.py
@@ -13,12 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/clement/9h37/raven/database.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db_test',                      # Or path to database file if using sqlite3.
+        'USER': 'test',                      # Not used with sqlite3.
+        'PASSWORD': 'pswd',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -162,7 +162,7 @@ LOGGING = {
         }
     },
     'loggers': {
-	'django': {
+	   'django': {
             'handlers':['console'],
             'propagate': True,
             'level':'INFO',
